6883495fede0e40221e2edf1656de760225499f9
wangjun008xx <wangjun009xx@hotmail.com>
2016-02-15 10:00:12 +0800

    add device confirms

---
+++ src/main/java/com/xst/bigwhite/dtos/ScanInputType.java
@@ -0,0 +1,17 @@
+package com.xst.bigwhite.dtos;
+
+/**
+ * 扫描设备号或则输入设备号
+ * @author wangjun
+ *
+ */
+public enum ScanInputType{
+	/**
+	 *  扫描二维码
+	 */
+	ScanQR,
+	/**
+	 * 输入文字
+	 */
+	InputText
+}
