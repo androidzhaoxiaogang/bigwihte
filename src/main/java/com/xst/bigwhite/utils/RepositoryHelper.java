64d7a45bf55dab8e2dccc4e8a5a4ed6a5667f165
Administrator <Administrator@WJ7.qkcorp.qiku.com>
2016-03-14 10:49:55 +0800

    deviceno

---
+++ src/main/java/com/xst/bigwhite/utils/RepositoryHelper.java
@@ -0,0 +1,177 @@
+package com.xst.bigwhite.utils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import com.mysema.query.jpa.impl.JPAQuery;
+import com.mysema.query.types.expr.BooleanExpression;
+import com.xst.bigwhite.models.AccountDevice;
+import com.xst.bigwhite.models.ConferenceAccount;
+import com.xst.bigwhite.models.QAccount;
+import com.xst.bigwhite.models.QAccountDevice;
+import com.xst.bigwhite.models.QConference;
+import com.xst.bigwhite.models.QConferenceAccount;
+import com.xst.bigwhite.models.QDevice;
+
+public class RepositoryHelper {
+
+	public static Iterable<ConferenceAccount> getAccountConferenceByDeviceno(EntityManager entityManager,String no) {
+		QConferenceAccount qConferenceAccount = QConferenceAccount.conferenceAccount;
+		QAccount qAccount = QAccount.account;
+		QConference qConference = QConference.conference;
+		QDevice qDevice = QDevice.device;
+		BooleanExpression device = qDevice.no.eq(no);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<ConferenceAccount> accountdevices = query.from(qConferenceAccount)
+				.leftJoin(qConferenceAccount.account, qAccount).fetch()
+				.leftJoin(qConferenceAccount.conference, qConference).fetch().leftJoin(qConference.device, qDevice)
+				.fetch().where(device).list(qConferenceAccount);
+
+		return accountdevices;
+	}
+
+	
+	public static Iterable<AccountDevice> getAccountByDeviceno(EntityManager entityManager,String deviceno) {
+		QAccountDevice qAccountDevice = QAccountDevice.accountDevice;
+		QDevice qDevice = QDevice.device;
+		QAccount qAccount = QAccount.account;
+		BooleanExpression bDeviceno = qDevice.no.eq(deviceno);
+		// Iterable<AccountDevice> accountList =
+		// accountDeviceRepository.findAll(bDeviceno);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<AccountDevice> accountdevices = query.from(qAccountDevice).leftJoin(qAccountDevice.account, qAccount)
+				.fetch().leftJoin(qAccountDevice.device, qDevice).fetch().where(bDeviceno).list(qAccountDevice);
+
+		return accountdevices;
+	}
+	
+	
+	public static Iterable<AccountDevice> getAccountDeviceMaster(EntityManager entityManager, String deviceno) {
+		QAccountDevice qAccountDevice = QAccountDevice.accountDevice;
+		QAccount qAccount = QAccount.account;
+		QDevice qDevice = QDevice.device;
+		
+		BooleanExpression deviceMobile = qDevice.no.eq(deviceno);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<AccountDevice> accountdevices = query.from(qAccountDevice)
+			 .leftJoin(qAccountDevice.account,qAccount).fetch()
+			 .leftJoin(qAccountDevice.device,qDevice).fetch()
+			 .where(deviceMobile.and(qAccountDevice.devicemaster.eq(true)))
+			 .list(qAccountDevice);
+	
+		return accountdevices;
+	}
+	
+	public static Iterable<AccountDevice> getAccountDevice(EntityManager entityManager,String mobileno , String deviceno) {
+		QAccountDevice qAccountDevice = QAccountDevice.accountDevice;
+		QAccount qAccount = QAccount.account;
+		QDevice qDevice = QDevice.device;
+		
+		BooleanExpression accountMobile = qAccount.mobileno.eq(mobileno);
+		BooleanExpression deviceMobile = qDevice.no.eq(deviceno);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<AccountDevice> accountdevices = query.from(qAccountDevice)
+			 .leftJoin(qAccountDevice.account,qAccount).fetch()
+			 .leftJoin(qAccountDevice.device,qDevice).fetch()
+			 .where(accountMobile.and(deviceMobile))
+			 .list(qAccountDevice);
+	
+		return accountdevices;
+	}
+
+	/*
+	public static Iterable<AccountDevice> getAccountDevice(EntityManager entityManager,String mobileno , String deviceno) {
+		QAccountDevice qAccountDevice = QAccountDevice.accountDevice;
+		QAccount qAccount = QAccount.account;
+		QDevice qDevice = QDevice.device;
+		
+		BooleanExpression accountMobile = qAccount.mobileno.eq(mobileno);
+		BooleanExpression deviceMobile = qDevice.no.eq(deviceno);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<AccountDevice> accountdevices = query.from(qAccountDevice)
+			 .leftJoin(qAccountDevice.account,qAccount).fetch()
+			 .leftJoin(qAccountDevice.device,qDevice).fetch()
+			 .where(accountMobile.and(deviceMobile))
+			 .list(qAccountDevice);
+	
+		return accountdevices;
+	}*/
+	
+	public static Iterable<AccountDevice> getAccountDeviceByDeviceno(EntityManager entityManager,String no) {
+		QAccountDevice qAccountDevice = QAccountDevice.accountDevice;
+		QAccount qAccount = QAccount.account;
+		QDevice qDevice = QDevice.device;
+		
+		BooleanExpression device = qDevice.no.eq(no);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<AccountDevice> accountdevices = query.from(qAccountDevice)
+			 .leftJoin(qAccountDevice.account,qAccount).fetch()
+			 .leftJoin(qAccountDevice.device,qDevice).fetch()
+			 .where(device)
+			 .list(qAccountDevice);
+	
+		return accountdevices;
+	}
+
+	
+	public static Iterable<AccountDevice> getAccountDeviceByAccountMobile(EntityManager entityManager,String mobileno) {
+		QAccountDevice qAccountDevice = QAccountDevice.accountDevice;
+		QAccount qAccount = QAccount.account;
+		QDevice qDevice = QDevice.device;
+		
+		BooleanExpression account = qAccount.mobileno.eq(mobileno);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<AccountDevice> accountdevices = query.from(qAccountDevice)
+			 .leftJoin(qAccountDevice.account,qAccount).fetch()
+			 .leftJoin(qAccountDevice.device,qDevice).fetch()
+			 .where(account)
+			 .list(qAccountDevice);
+	
+		return accountdevices;
+	}
+
+	public static Iterable<AccountDevice> getAccountDeviceMasterByDeviceNo(EntityManager entityManager,String deviceno) {
+		QAccountDevice qAccountDevice = QAccountDevice.accountDevice;
+		QAccount qAccount = QAccount.account;
+		QDevice qDevice = QDevice.device;
+		
+		BooleanExpression deviceMobile = qDevice.no.eq(deviceno);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<AccountDevice> accountdevices = query.from(qAccountDevice)
+			 .leftJoin(qAccountDevice.account,qAccount).fetch()
+			 .leftJoin(qAccountDevice.device,qDevice).fetch()
+			 .where(deviceMobile.and(qAccountDevice.devicemaster.eq(true)))
+			 .list(qAccountDevice);
+	
+		return accountdevices;
+	}
+
+	public static Iterable<ConferenceAccount> getAccountConferenceByAccountMobile(EntityManager entityManager,String mobileno) {
+		QConferenceAccount qConferenceAccount = QConferenceAccount.conferenceAccount;
+		QAccount qAccount = QAccount.account;
+		QConference qConference = QConference.conference;
+		QDevice qDevice = QDevice.device;
+		
+		BooleanExpression account = qAccount.mobileno.eq(mobileno);
+
+		JPAQuery query = new JPAQuery(entityManager);
+		Iterable<ConferenceAccount> accountdevices = query.from(qConferenceAccount)
+			 .leftJoin(qConferenceAccount.account,qAccount).fetch()
+			 .leftJoin(qConferenceAccount.conference,qConference).fetch()
+			 .leftJoin(qConference.device,qDevice).fetch()
+			 .where(account)
+			 .list(qConferenceAccount);
+	
+		return accountdevices;
+	}
+
+	
+}
